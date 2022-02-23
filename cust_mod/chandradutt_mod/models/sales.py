3376e05e991a97fd00cdab1acbb92fe3d84ddeb4
odoo <chandradutt@icreativetechnolabs.com>
2022-02-23 10:15:31 +0530

    [ADD]asdf

--- cust_mod/chandradutt_mod/models/sales.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-
-class sale(models.Model):
-    _inherit = "sale.order"
-
-    ddaa = fields.Date()
--- cust_mod/first_mod/models/sales.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class sales(models.Model):
-    _inherit = 'sale.order'
-
-    date1 = fields.Date()
