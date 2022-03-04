b3b394fdab133074ce462dec12ee1d7e575d7242
odoo <chandradutt@icreativetechnolabs.com>
2022-03-04 18:26:51 +0530

    [ADD]worked on yusuf sir and foram mam tasks all completed

--- cust_mod/hospital_m/models/doctor.py
+++ cust_mod/hospital_m/models/doctor.py
@@ -6,6 +6,7 @@ class doctor(models.Model):
     _rec_name = 'd_speciality'
     _inherit = 'mail.activity.mixin'
     
+    img = fields.Binary()
     d_id = fields.Integer()
     d_name = fields.Char()
     d_dob = fields.Date()
@@ -14,14 +15,9 @@ class doctor(models.Model):
                                      ('family physician','Family physician'),
                                      ('plastic surgeons','Plastic Surgeons')])
     d_gender = fields.Selection([('male','Male'),('female','Female'),('other','Other')])
-    patient_name = fields.Many2one('hospital_m.hospital_m',string='student id')
+    patient_name = fields.Many2one('hospital_m.hospital_m',string='Patient_Name')
+    date = fields.Datetime('Date current action'
+                           , default=lambda self: fields.datetime.now())
     # check_list = fields.Integer(compute='check_list', string='progress',store=True)
 
-    # project_progress = fields.Float(
-    #     compute='_compute_project_progress_by_user', string='Project Progress')
-
     
-    # def pro(self):
-    #     check_list=int(input('enter value'))
-    #     print(check_list)
-    # print(pro(check_list))
--- cust_mod/hospital_m/models/models.py
+++ cust_mod/hospital_m/models/models.py
@@ -7,29 +7,37 @@ class hospital_m(models.Model):
     _name = 'hospital_m.hospital_m'
     _rec_name = 'fname'
     _description = 'hospital_m.hospital_m'
-    _inherit = 'mail.thread','mail.activity.mixin'
+    _inherit = 'mail.thread', 'mail.activity.mixin'
 
     # p_id = fields.Integer()
     fname = fields.Char()
     lname = fields.Char()
-    gender = fields.Selection([('male','Male'),('female','Female')])
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')])
     mobile = fields.Integer()
     email = fields.Char()
     dateofbirth = fields.Date()
     image = fields.Binary()
     address = fields.Text()
-    blood_group = fields.Selection([('a','A'),('a+','A+'),('a-','A-'),
-                                    ('b','B'),('b+','B+'),('b-','B-'),
-                                    ('ab','AB'),('ab+','AB+'),('ab-','AB-'),
-                                    ('o','O'),('o+','O+'),('o-','O-')])
+    blood_group = fields.Selection([('a', 'A'), ('a+', 'A+'), ('a-', 'A-'),
+                                    ('b', 'B'), ('b+', 'B+'), ('b-', 'B-'),
+                                    ('ab', 'AB'), ('ab+', 'AB+'), ('ab-', 'AB-'),
+                                    ('o', 'O'), ('o+', 'O+'), ('o-', 'O-')])
+    bg = fields.Char(readonly=True)
     v_charge = fields.Float()
     disease = fields.Char()
     a_date = fields.Date()
-    doc_name = fields.Many2one('doctor.doctor',string='doctor name')
-    doc_names = fields.Many2many('doctor.doctor',string='doctors')
+    doc_type = fields.Many2one('doctor.doctor', string='doctor type')
+    doc_names = fields.Many2many('doctor.doctor', string='doctors')
+    state = fields.Selection([('patient_details', 'Patient_details'), ('appointment_status', 'Appointment_status')],
+                             default="patient_details", string="status_bar")
     # description = fields.Text()
 
-
+    
+    @api.onchange('blood_group')
+    def bl_g(self):
+        for rec in self:
+            if(rec.blood_group):
+                rec.bg = rec.blood_group
 
     @api.depends('value')
     def print(self):
